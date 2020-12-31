e9be46a42818a7a87cb720f23d3e153e10ebf862
sahilkumar25 <ramagirisahil@gmail.com>
2020-12-31 23:13:56 +0530

    added get functionality to articles

--- back-end/src/soulsion_api/articles/routes.py
+++ back-end/src/soulsion_api/articles/routes.py
@@ -1,4 +1,5 @@
 from flask import (
+    abort,
     Blueprint,
     request
 )
@@ -14,16 +15,29 @@ __article_data_directory = "/data/article/" #folder to articles data (file)
 def get_article():
 
     # get article id  from request data
+    article_id = request.form.get('article_id', '')
 
     # validate data
+    article = Articles.query.filter_by(article_id = article_id).first()
+    if article is None:
+        abort(404, description="article not found")
 
     # get the file name corresponding to the article in the database : Articles -> say "stress-management.md"
+    file_name = article.file_name
 
     # read the contents of the file -> __article_data_directory + file_name -> /data/article/stress-management.md
-
+    full_path = __article_data_directory + file_name
+    
+    try:
+        with open(full_path, 'r') as file:
+            article_content = file.read()
+    except (OSError, IOError):
+        abort(404, description="file not found")
+    
     # return the content
     data = {
-        "content":"Article content"
+        "title": article.title,
+        "content": article_content
     }
 
     return data
