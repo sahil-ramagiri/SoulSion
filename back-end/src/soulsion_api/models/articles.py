a8acd8b98770d35370cf7aba49dcf21c45c9a316
sahilkumar25 <ramagirisahil@gmail.com>
2020-12-31 23:17:23 +0530

    added db model for articles

---
+++ back-end/src/soulsion_api/models/articles.py
@@ -0,0 +1,12 @@
+
+
+from soulsion_api import db
+
+
+class Articles(db.Model):
+    article_id = db.Column(db.String(20), primary_key=True)
+    file_name = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Articles %r>' % self.article_id
