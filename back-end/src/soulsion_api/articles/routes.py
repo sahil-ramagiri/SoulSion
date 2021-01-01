2fd494c4701828f5cb5e1b17129c831212c9219d
sahilkumar25 <ramagirisahil@gmail.com>
2021-01-01 11:58:22 +0530

    removed commnets

--- back-end/src/soulsion_api/articles/routes.py
+++ back-end/src/soulsion_api/articles/routes.py
@@ -7,34 +7,30 @@ from flask import (
 from soulsion_api.utils.utils import get_random_file_name
 from soulsion_api.models.articles import Articles
 
-articles = Blueprint('articles',__name__)
+articles = Blueprint('articles', __name__)
 
-__article_data_directory = "/data/article/" #folder to articles data (file)
+__article_data_directory = "/data/article/"
 
-@articles.route('/api/articles/get',methods=['POST'])
+
+@articles.route('/api/articles/get', methods=['POST'])
 def get_article():
 
-    # get article id  from request data
     article_id = request.form.get('article_id', '')
 
-    # validate data
-    article = Articles.query.filter_by(article_id = article_id).first()
+    article = Articles.query.filter_by(article_id=article_id).first()
     if article is None:
         abort(404, description="article not found")
 
-    # get the file name corresponding to the article in the database : Articles -> say "stress-management.md"
     file_name = article.file_name
 
-    # read the contents of the file -> __article_data_directory + file_name -> /data/article/stress-management.md
     full_path = __article_data_directory + file_name
-    
+
     try:
         with open(full_path, 'r') as file:
             article_content = file.read()
     except (OSError, IOError):
         abort(404, description="file not found")
-    
-    # return the content
+
     data = {
         "title": article.title,
         "content": article_content
@@ -42,6 +38,7 @@ def get_article():
 
     return data
 
-@articles.route('/api/articles/post',methods=['POST'])
+
+@articles.route('/api/articles/post', methods=['POST'])
 def post_article():
     pass
